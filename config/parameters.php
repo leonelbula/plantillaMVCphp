0bd6753cbeb3860a0b735dd783e8108fdfcd8bc0
leonelbula <lbgleonel_007@hotmail.com>
2019-10-26 11:28:25 -0500

    plantilla base

---
+++ config/parameters.php
@@ -0,0 +1,9 @@
+<?php
+//ruta principal del proyecto
+define("URL_BASE", "http://localhost/blog_basico/frontend/");
+
+//controlador principal
+define("CONTROLLER_DEFAULT", "frontendController");
+
+//acion principal del controlador 
+define("ACTION_DEFAULT", "index");
---
+++ controllers/frontendController.php
@@ -0,0 +1,14 @@
+<?php
+
+class frontendController{
+	
+	public function index() {
+		
+		require_once 'views/layout/menu.php';		
+		require_once 'views/layout/principal.php';
+		require_once 'views/layout/footer.php';
+		
+	}	
+	
+}
+
